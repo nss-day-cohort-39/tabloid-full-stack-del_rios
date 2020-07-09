63dfd852ecc5f0277056c6d1f800ffbdceb4aaaa
Keartroth <mecarrolljr@gmail.com>
2020-07-09 11:48:09 -0500

    - Added server side repository, controller, and post model.

---
+++ Tabloid/Models/Post.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Tabloid.Models
+{
+    public class Post
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string Content { get; set; }
+
+        public string ImageLocation { get; set; }
+
+        [Required]
+        public DateTime CreateDateTime { get; set; }
+
+        public DateTime PublishDateTime { get; set; }
+
+        [Required]
+        public bool IsApproved { get; set; }
+
+        [Required]
+        public int CategoryId { get; set; }
+
+        [Required]
+        public int UserProfileId { get; set; }
+
+        public UserProfile UserProfile { get; set; }
+
+    }
+}
