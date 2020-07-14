3f3e96efdbdf2f5886b775122d48dcf9dec733ca
unknown <buckleyderekp@gmail.com>
2020-07-14 11:28:34 -0500

    backend for userProfile

--- Tabloid/Controllers/UserProfileController.cs
+++ Tabloid/Controllers/UserProfileController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Infrastructure;
 using System;
+using System.Security.Claims;
 using Tabloid.Data;
 using Tabloid.Models;
 using Tabloid.Repositories;
@@ -33,6 +34,45 @@ namespace Tabloid.Controllers
                 nameof(GetUserProfile),
                 new { firebaseUserId = userProfile.FirebaseUserId },
                 userProfile);
+        }
+
+        [HttpGet("getallusers")]
+        public IActionResult GetAllUsers()
+        {
+            var userProfile = GetCurrentUserProfile();
+            if(userProfile.UserTypeId == 1)
+            {
+            return Ok(_userProfileRepository.GetAll());
+            }
+            else
+            {
+                return Unauthorized();
+            }
+        }
+        private UserProfile GetCurrentUserProfile()
+        {
+            var firebaseUserId = User.FindFirst(ClaimTypes.NameIdentifier).Value;
+            return _userProfileRepository.GetByFirebaseUserId(firebaseUserId);
+        }
+
+        [HttpPut("updateuserprofile/{id}")]
+        public IActionResult UpdateUserProfile(UserProfile userProfile, int id)
+        {
+            var currentUserProfile = GetCurrentUserProfile();
+            if (currentUserProfile.UserTypeId == 1)
+            {
+                if (id != userProfile.Id)
+                {
+                return BadRequest();
+                }
+
+                _userProfileRepository.Update(userProfile);
+                return NoContent();               
+            }
+            else
+            {
+                return Unauthorized();
+            }
         }
     }
 }
